9dc87eaf20a3900d2faf1668590a71667186f51a
Doug Neiner <doug.neiner@leankit.com>
2014-11-12 09:04:06 -0600

    Filled out stubbed tests for luxComponent

--- spec/luxComponent.spec.js
+++ spec/luxComponent.spec.js
@@ -1,62 +1,107 @@
-/* global describe, it, before, lux, utils, luxActionCh */
+/* global describe, it, before, lux, React, utils, luxActionCh */
+
+var _ = require( "lodash" );
+
+function verifyAction( name, args ) {
+	args = args || [];
+	luxActionCh.subscribe("execute." + name, function(data) {
+		data.should.have.property("actionType", name );
+		data.should.have.property("actionArgs");
+		args.forEach( function ( arg, i) {
+			data.actionArgs[i].should.eql( arg );
+		});
+	}).once();
+}
 
 describe( "luxJS - components", function() {
-	var fakeStore;
+	var Component, innerRef, mocked, fakeStore;
+
+	function componentFactory( options ) {
+		options = Object.assign( {
+			displayName: "testComponent",
+			componentWillMount: function() {
+				innerRef = this;
+			},
+			render: function() {
+				return null;
+			}
+		}, options );
+
+		Component = lux.component( options );
+		mocked = utils.renderIntoDocument( React.createElement( Component ) );
+	}
+
 	before(function(){
 		fakeStore = new lux.Store({
 			namespace: "testytesttest",
 			handlers: {
 				doYourLuxThing: function() {
 					return { yay: true };
+				},
+				doAnotherThing: function () {
+					return { sweet: true };
+				},
+				notInGroup: function () {
+					return { emo: true };
 				}
 			}
 		});
 		lux.addToActionGroup("testyactiongroup", [ "doYourLuxThing" ]);
+		lux.addToActionGroup("testyextragroup", [ "doYourLuxThing", "doAnotherThing" ]);
 	});
 	describe( "When Calling getActionGroup", function(){
 		it("should return expected actions for given action group", function() {
 			lux.getActionGroup("testyactiongroup").should.have.property("doYourLuxThing");
 		});
 	} );
-    describe( "When Instantiating a lux Component", function() {
-    	describe( "When using getActionGroup for one group", function() {
-    		var Component;
-	    	var innerRef;
-	    	var mocked;
-	    	before(function(){
-	    		Component = lux.component({
-	    			displayName: "testComponent",
-	    			getActionGroup: ["testyactiongroup"],
-	    			componentWillMount: function() {
-	    				innerRef = this;
-	    			},
-	    			render: function() {
-	    				return null;
-	    			}
-	    		});
-	    		mocked = utils.renderIntoDocument( Component() );
-	    	});
-	    	it("Should provide a `doYourLuxThing` method to the component", function() {
-	    		innerRef.should.have.property("doYourLuxThing");
-	    	});
-	    	it("Should publish expected message when invoking action method", function(done) {
-	    		luxActionCh.subscribe("execute.doYourLuxThing", function(data) {
-	    			data.should.have.property("actionType", "doYourLuxThing");
-	    			data.should.have.property("actionArgs");
-	    			data.actionArgs[0].should.eql("foo");
-	    			data.actionArgs[1].should.eql(8675309);
-	    			done();
-	    		}).once();
-	    		innerRef.doYourLuxThing("foo", 8675309);
-	    	});
-    	});
-    	describe( "When using getActionGroup for multiple groups", function() {
-    		it("Should provide the correct methods to the component", function() {});
-    		it("Should publish expected message when invoking action method", function() {});
-    	});
-    	describe( "When using getAction", function() {
-    		it("Should provide the correct method(s) to the component", function() {});
-    		it("Should publish expected message when invoking action method", function() {});
-    	});
-    });
+	describe( "When Instantiating a lux Component", function() {
+		describe( "When using getActionGroup for one group", function() {
+			before(function(){
+				componentFactory({
+					getActionGroup: ["testyactiongroup"]
+				});
+			});
+			it("Should provide a `doYourLuxThing` method to the component", function() {
+				innerRef.should.have.property("doYourLuxThing");
+			});
+			it("Should publish expected message when invoking action method", function() {
+				verifyAction( "doYourLuxThing", [ "foo", 8675309 ]);
+				innerRef.doYourLuxThing("foo", 8675309);
+			});
+		});
+		describe( "When using getActionGroup for multiple groups", function() {
+			before(function(){
+				componentFactory({
+					getActionGroup: [ "testyactiongroup", "testyextragroup" ]
+				});
+			});
+			it("Should provide the correct methods to the component", function() {
+				innerRef.should.have.property("doYourLuxThing");
+				innerRef.should.have.property("doAnotherThing");
+			});
+			it("Should publish expected message when invoking action method", function () {
+				verifyAction( "doYourLuxThing", [ "foo", 8675309 ]);
+				verifyAction( "doAnotherThing", [ "bar", 8675310 ]);
+				innerRef.doYourLuxThing("foo", 8675309);
+				innerRef.doAnotherThing("bar", 8675310);
+			});
+		});
+		describe( "When using getActions", function() {
+			before(function(){
+				componentFactory({
+					getActions: [ "doAnotherThing", "notInGroup" ]
+				});
+			});
+			it("Should provide the correct method(s) to the component", function () {
+				innerRef.should.have.property("doAnotherThing");
+				innerRef.should.have.property("notInGroup");
+			});
+			it("Should publish expected message when invoking action method", function () {
+				verifyAction( "doAnotherThing", [ "bar", 8675310 ]);
+				verifyAction( "notInGroup", [ "foo", 8675309 ]);
+				innerRef.doAnotherThing("bar", 8675310);
+				innerRef.notInGroup("foo", 8675309);
+			});
+		});
+	});
 });
